9310cc3e68e7ec197eeecfba8f245e8c644f1cf2
devjvla <devjvla@gmail.com>
2025-07-24 15:46:27 +0800

    v2

--- finalAssignmentMVC/server.js
+++ finalAssignmentMVC/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
-
+const bodyParser = require('body-parser');  
 app.use('/', userRoutes);
 
 app.listen(8000, () => {
